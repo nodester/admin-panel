67367377b4bf2aa433be697d1f726702dcafd209
mike <mike@Deepa-Shahs-iMac.local>
2011-04-27 15:51:15 +0530

    added auth

---
+++ auth.js
@@ -0,0 +1,28 @@
+exports.auth = function(options) {
+  options= options || {};
+  var auth= {};
+	// my auth name
+  auth.name     = options.name || "awesomeauth";
+	
+	// validate_creds
+ 	function validate_credentials( executionScope, request, response, callback ) {
+		if( request.user.user == 'foo' && request.user.password == 'bar' ) {
+			console.log('s');
+      executionScope.success( {name:request.user.user}, callback )
+    } else {
+			console.log('f');
+      executionScope.fail( callback )
+    }
+  };
+
+	// expose this method
+  auth.authenticate = function(req, res, callback) {
+		if( req.user && req.user.user && req.user.password ) { 
+		  validate_credentials( this, req, res, callback );
+		} else {
+		  res.end('dead');
+		}
+  }
+
+  return auth;
+};
--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-var express = require('express');
+var http = require('http'),
+		express = require('express'),
+		encode = require("./encoding"),
+		cauth= require('connect-auth'),
+		auth = require('./auth');
 
 var app = module.exports = express.createServer();
 
 // Configuration
-
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+	app.use(express.static(__dirname + '/public'));
+	app.use(cauth(auth.auth())); // connect-auth with my custom auth
+	app.use(express.logger()); // enable logger
+  app.use(express.bodyParser()); // parse body
+  app.use(express.methodOverride()); // ??
+	app.use(express.cookieParser()); // cookie parser
+	app.use(express.session({key :"ns",secret: "keyboard cat" })); // session store (NOTE cookieParser should be b4 this)
+  app.use(app.router); // use the router
 });
 
 app.configure('development', function(){
@@ -21,16 +28,104 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
-// Routes
+// Checks whether user has logged in
+// No data store used, just plain cookie based auth
+function checkAuth(req,res,next) {
+	// if key=>cred is present in session
+	// then user is logged in
+	// --- Need to write logged in session 
+	// after verification frm nodester
+	if(req.session.cred) {
+		// get from session
+		console.log('from session');
+		req.user = req.session.cred;
+	} else {
+		console.log('to session');
+		req.user = {
+			user: "123",
+			password: "bar"
+		};
+		// store to session
+		req.session.cred = req.user;
+	}
+	// authenticate method, where req,res is passed and callback is passed
+	req.authenticate(['awesomeauth'], function(err, authenticated) {
+		console.log(authenticated);
+  });
+	next();
+}
 
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
+// Logout
+app.get('/d',checkAuth, function(req,res) {
+	// check whether user is logged in ?
+	// then log him out
+	req.session.destroy(); // destroy cookie session created
+	res.redirect("/"); // redirect to home after delete
+})
+
+// Routes
+app.get('/', checkAuth, function(req, res){
+	// give auth name
+	res.render('index', {
+    title: 'Nodester Admin Panel'
   });
 });
 
-// Only listen on $ node app.js
+// Need to write paths to all ndoester APIs
+// including GET and POST
+// Need to figure out REGEX
+app.get('/:path', function(req, res, next){
+	console.log("body ==> ",req.body);
+	console.log("query ==> ",req.query);
+	console.log("params ==> ",req.params);
+	
+	proxyToNodester("GET", req.params.path, req.query, function(data) {
+		res.header('Content-Type', 'text/plain');
+		res.end(data);
+	});
+	
+});
 
+// Generate Query parameters from params(json)
+function generateQueryParams(params) {
+  tail = [];
+  for (var p in params) {
+    if (params.hasOwnProperty(p)) {
+      tail.push(p + "=" + encodeURIComponent(params[p]));
+    }
+  }
+	if(tail.length > 0)
+  	return "?" + tail.join("&");
+	else
+		return "";
+}
+
+// Proxy to api.nodester.com 
+function proxyToNodester(method, path, data, callback) {
+	var formattedPath = "/" + path + generateQueryParams(data);
+	console.log("formatted path ===> ", formattedPath);
+	var options = {
+	  host: 'api.nodester.com',
+	  port: 80,
+	  path: formattedPath,
+		headers: {"Authorization" : "Basic " + encode.base64("rowoot:hackerro")},
+		method: method
+	};
+	
+	var req = http.request(options, function(res) {
+	  console.log('STATUS: ' + res.statusCode);
+	  console.log('HEADERS: ' + JSON.stringify(res.headers));
+	  res.setEncoding('utf8');
+	  res.on('data', function (chunk) {
+	    //console.log('BODY: ' + chunk);
+			if(typeof(callback) == "function")
+				callback(chunk);
+	  });
+	});
+	req.end();
+}
+
+// Only listen on $ node app.js
 if (!module.parent) {
   app.listen(9651);
   console.log("Express server listening on port %d", app.address().port);
