eded5ac5aae95ff063cc71dd4aac31e6cea1a8a6
mike <mike@Deepa-Shahs-iMac.local>
2011-04-27 11:51:27 +0530

    express framework

--- server.js
+++ server.js
@@ -1,5 +1,37 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (admin) is running..');
-}).listen(9651);
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+// Only listen on $ node app.js
+
+if (!module.parent) {
+  app.listen(9651);
+  console.log("Express server listening on port %d", app.address().port);
+}
