87908194265b669b0099e0ed4f80793ee42c5c0e
Aaron Kavlie <akavlie@gmail.com>
2012-02-25 21:31:37 -0700

    Basic skeleton with many of the needed models/collections/views.

---
+++ public/javascripts/main.js
@@ -0,0 +1,35 @@
+(function() {
+
+// Models & Collections
+// --------------------
+
+var App = Backbone.Model.extend({
+	// properties: name, port, status
+	// actions: start, stop, show logs, show info, destroy
+});
+
+var Apps = Backbone.Collection.extend({
+	model: App
+});
+
+
+// Views
+// -----
+var AppView = Backbone.Views.extend({
+});
+
+var AppListView = Backbone.Views.extend({
+});
+
+var DomainListView = Backbone.Views.extend({
+});
+
+var MainView = Backbone.Views.extend({
+});
+
+
+$(function() {
+	new MainView;
+});
+
+})();
